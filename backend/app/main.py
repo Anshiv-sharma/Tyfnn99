7e860eab1769c464253558903e413b2453cfa01e
Anshiv Sharma <anshivsharma07@gmail.com>
2026-07-20 21:08:14 +0530

    Implemented secure user registration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db_session
 from app.schemas import UserCreate
 from app.models import User 
+from app.utils import hash_password
 
 app = FastAPI()
 
@@ -16,5 +17,39 @@ def register(
     user: UserCreate,
     db: Session = Depends(get_db_session)
 ):
-    print(user)
-    return{"message": "Registeration request received!"}
+
+    # checking if the email alr exists
+    existing_email = db.query(User).filter(User.email == user.email).first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+    
+    # checking if the phone no. alr exists
+    existing_phone = db.query(User).filter(User.phone == user.phone).first()
+
+    if existing_phone:
+        raise HTTPException(
+            status_code=400,
+            detail="Phone number already registered"
+        )
+    
+    # creating a new user
+    new_user = User(
+        name = user.name,
+        email = user.email,
+        phone = user.phone,
+        password_hash = hash_password(user.password)
+    )
+
+    #save to database
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return{
+        "message": "User registered successfully!",
+        "user_id": new_user.id
+        }
